8ae37e2e495bae641afe2031994dadf08ba173d7
WalneiOliveira <woliveiraassis@gmail.com>
2025-06-28 17:45:24 -0300

    add menu hamburguer

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 // File: script.js
 //efeito de carregamento de página
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const loadingScreen = document.querySelector('.loading-screen');
   loadingScreen.style.display = 'none'; // Esconde a tela de carregamento após o carregamento do DOM
 });
 
 // Efeito de carregamento das seções com pequeno delay
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
   const sections = document.querySelectorAll('section');
   sections.forEach((section, index) => {
     section.style.opacity = 0;
@@ -17,6 +17,34 @@ document.addEventListener('DOMContentLoaded', function() {
   });
 });
 
-
 // Define o ano atual no rodapé
 document.getElementById('year').textContent = new Date().getFullYear();
+
+// Menu Lateral Responsivo
+document.addEventListener('DOMContentLoaded', function () {
+  const hamburgerMenu = document.getElementById('hamburger-menu');
+  const sidebar = document.querySelector('.sidebar');
+  const sidebarOverlay = document.getElementById('sidebar-overlay');
+
+  hamburgerMenu.addEventListener('click', function () {
+    sidebar.classList.toggle('open');
+    hamburgerMenu.classList.toggle('open');
+  });
+
+  sidebarOverlay.addEventListener('click', function () {
+    sidebar.classList.remove('open');
+    hamburgerMenu.classList.remove('open');
+  });
+});
+// Efeito de clique no menu lateral
+document.addEventListener('DOMContentLoaded', function () {
+  const sidebarLinks = document.querySelectorAll('.sidebar-nav a');
+
+  sidebarLinks.forEach(link => {
+    link.addEventListener('click', function () {
+      sidebarLinks.forEach(l => l.classList.remove('active'));
+      this.classList.add('active');
+    });
+  });
+});
+
